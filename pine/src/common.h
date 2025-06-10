a5eba3fa569ea69d9391b2072f072cc2a68e5d18
Lucius Landschulze <lucland278@gmail.com>
2025-06-10 23:30:15 +0200

    Add TODO's

--- pine/src/common.h
+++ pine/src/common.h
@@ -18,9 +18,11 @@ struct PxContext_internal {
 
 void PxiUpdateTitle(PxContext *context, PxWindow *window, const char *new_title);
 
+// TODO: Merge PxiUpdateX and PxiUpdateY into single function
 void PxiUpdateX(PxContext *context, PxWindow *window, int new_x);
 void PxiUpdateY(PxContext *context, PxWindow *window, int new_y);
 
+// TODO: Merge PxiUpdateWidth and PxiUpdateHeight into single function
 void PxiUpdateWidth(PxContext *context, PxWindow *window, int new_width);
 void PxiUpdateHeight(PxContext *context, PxWindow *window, int new_height);
 #endif //_COMMON_H
